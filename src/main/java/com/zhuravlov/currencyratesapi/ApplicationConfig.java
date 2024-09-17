8006ce10c49dc9831ad59cc87cf3e34c42724618
andrii zhuravlov <a.v.zhuravlov@gmail.com>
2024-09-17 09:39:10 +0200

    Set UTC time zine as default

--- src/main/java/com/zhuravlov/currencyratesapi/ApplicationConfig.java
+++ src/main/java/com/zhuravlov/currencyratesapi/ApplicationConfig.java
@@ -1,10 +1,19 @@
 package com.zhuravlov.currencyratesapi;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @Configuration
 @EnableJdbcRepositories
+@EnableScheduling
 public class ApplicationConfig {
 
+    @PostConstruct
+    void setUTCTimeZone() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 }
